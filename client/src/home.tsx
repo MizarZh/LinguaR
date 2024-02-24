3a6a0955371c06acb8df7e302772b4cf52bbd287
MizarZh <2267243023@qq.com>
2024-02-24 02:09:33 +0100

    feat: Home init

--- client/src/home.tsx
+++ client/src/home.tsx
@@ -1,7 +1,27 @@
 import React, { useState, useContext } from 'react'
 
 function Home() {
-  return <div>Hello world!</div>
+  const series = ['test1', 'test2']
+  const lists = [
+    ['article 1', 'article 2', 'article 3'],
+    ['article 4', 'article 5', 'article 6', 'article 4', 'article 5', 'article 6', 'article 4', 'article 5', 'article 6', 'article 4', 'article 5', 'article 6'],
+  ]
+  const output = series.map((val, idx) => {
+    const inner = lists[idx].map((val, idx) => {
+      return (
+        <div key={idx} className="m-10 basis-20 shrink-0">
+          {val}
+        </div>
+      )
+    })
+    return (
+      <div key={idx}>
+        <div className="flex flex-col">Title: {val}</div>
+        <div className="flex flex-row w-screen overflow-scroll">{inner}</div>
+      </div>
+    )
+  })
+  return <div>{output}</div>
 }
 
 export default Home
